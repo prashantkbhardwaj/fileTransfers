ee53a760ad6d2b1752c26605f1c5689aab2a1314
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-15 12:17:57 +0530

    video upload api added

---
+++ teqniHome/uploadVideo.php
@@ -0,0 +1,17 @@
+<?php
+ 
+ if($_SERVER['REQUEST_METHOD']=='POST'){
+ $file_name = $_FILES['myFile']['name'];
+ $file_size = $_FILES['myFile']['size'];
+ $file_type = $_FILES['myFile']['type'];
+ $temp_name = $_FILES['myFile']['tmp_name'];
+ 
+ $location = "uploads/";
+ 
+ move_uploaded_file($temp_name, $location.$file_name);
+ echo "http://192.168.1.101/fileTransfers/teqniHome/uploads/".$file_name;
+ }else{
+ echo "Error";
+ }
+
+ ?>
