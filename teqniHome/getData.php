af6384d2c58b433bc6c15a59cafbdc348a2cd4e2
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-18 00:01:46 +0530

    api and IP update

--- teqniHome/getData.php
+++ teqniHome/getData.php
@@ -7,7 +7,7 @@
 	$response = array();
 	while ($list = mysqli_fetch_assoc($result)) {
 		if ($list['timeDuration']=='0') {
-			$response[$i]['image'] = "http://192.168.1.104/fileTransfers/teqniHome/uploads/video.png";
+			$response[$i]['image'] = "http://192.168.1.102/fileTransfers/teqniHome/uploads/video.png";
 		} else {
 			$response[$i]['image'] = $list['imgPath'];
 		}
--- teqniHome/upload.php
+++ teqniHome/upload.php
@@ -15,7 +15,7 @@
 	 	date_default_timezone_set("Asia/Kolkata");
 		$dateUpload = date("d M, Y | h:i a");
 	 	$path = "uploads/$id.png";
-	 	$actualpath = "http://192.168.1.104/fileTransfers/teqniHome/$path";
+	 	$actualpath = "http://192.168.1.102/fileTransfers/teqniHome/$path";
 	 	$sql = "INSERT INTO volleyupload (imgPath, uploader, level1, level2, level3, pictureName, sessionName, timeDuration, dateUpload, qrcode) VALUES ('{$actualpath}','{$uploader}', '{$level1}', '{$level2}', '{$level3}', '{$pictureName}', '{$sessionName}', '{$timeDuration}', '{$dateUpload}', '{$qrcode}')";
 		if(mysqli_query($conn,$sql)){
 		 	file_put_contents($path,base64_decode($image));
