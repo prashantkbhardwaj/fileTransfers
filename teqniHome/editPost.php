7229e15e3734fd07c9bea241c5c495ffd3e32d67
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-17 23:49:54 +0530

    api update for edit post

---
+++ teqniHome/editPost.php
@@ -0,0 +1,19 @@
+<?php require_once("includes/db_connection.php");?>
+<?php require_once("includes/functions.php");?>
+
+<?php
+    if ($_POST['postid']!="") {
+        $postid = $_POST['postid']; 
+        $name = $_POST['name'];
+        $timeDuration = $_POST['timeDuration'];
+        
+        $query = "UPDATE volleyupload SET pictureName = '{$name}', timeDuration = '{$timeDuration}' WHERE id = '{$postid}' LIMIT 1";
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            $response = array();
+            $response["success"] = true;  
+            echo json_encode($response);     
+        } 
+    }
+?>
