ba9e9e7fb01d6c61eaa1e8133a565e7dbee8219e
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-18 14:28:43 +0530

    max pos test

--- teqniHome/upload.php
+++ teqniHome/upload.php
@@ -15,8 +15,14 @@
 	 	date_default_timezone_set("Asia/Kolkata");
 		$dateUpload = date("d M, Y | h:i a");
 	 	$path = "uploads/$id.png";
-	 	$actualpath = "http://192.168.1.102/fileTransfers/teqniHome/$path";
-	 	$sql = "INSERT INTO volleyupload (imgPath, uploader, level1, level2, level3, pictureName, sessionName, timeDuration, dateUpload, qrcode) VALUES ('{$actualpath}','{$uploader}', '{$level1}', '{$level2}', '{$level3}', '{$pictureName}', '{$sessionName}', '{$timeDuration}', '{$dateUpload}', '{$qrcode}')";
+
+	 	$query = "SELECT MAX(pos) FROM volleyupload";
+	 	$result = mysqli_query($conn, $query);
+	 	$list = mysqli_fetch_assoc($result);
+	 	$pos = $list['pos'] + 1;
+
+	 	$actualpath = "http://192.168.1.103/fileTransfers/teqniHome/$path";
+	 	$sql = "INSERT INTO volleyupload (imgPath, uploader, level1, level2, level3, pictureName, sessionName, timeDuration, dateUpload, qrcode, pos) VALUES ('{$actualpath}','{$uploader}', '{$level1}', '{$level2}', '{$level3}', '{$pictureName}', '{$sessionName}', '{$timeDuration}', '{$dateUpload}', '{$qrcode}', '{$pos}')";
 		if(mysqli_query($conn,$sql)){
 		 	file_put_contents($path,base64_decode($image));
 		 	echo "Successfully Uploaded";
