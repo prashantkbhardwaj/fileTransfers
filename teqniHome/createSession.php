c7e53115573f287d251816aabb7848b5a5fbf71c
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-08-23 23:45:32 +0530

    create session added

---
+++ teqniHome/createSession.php
@@ -0,0 +1,18 @@
+<?php
+ 	if($_SERVER['REQUEST_METHOD']=='POST'){
+ 
+	    $uploader = $_POST['uploader'];
+	    $level1 = $_POST['level1'];
+	    $level2 = $_POST['level2'];
+	    $level3 = $_POST['level3'];
+	    $sessionName = $_POST['sessionname'];
+	  	require_once("includes/db_connection.php");
+	 	$sql = "INSERT INTO volleyupload (uploader, level1, level2, level3, sessionName) VALUES ('{$uploader}', '{$level1}', '{$level2}', '{$level3}', '{$sessionName}')";
+		if(mysqli_query($conn,$sql)){
+		 	echo "Successfully Created";
+		}
+	 	mysqli_close($conn);
+	}else{
+	 	echo "Error";
+	}
+?>
