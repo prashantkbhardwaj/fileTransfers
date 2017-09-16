c75d5ffe5265b33ae69dec519499a02bb2ca63fc
Prashant Bhardwaj <pkpbhardwaj729@gmail.com>
2017-09-16 09:54:58 +0530

    ip change

--- teqniHome/test.php
+++ teqniHome/test.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-<script type="text/javascript">
-	var foo = "Hello";
- (function() {
- var bar = " World";
- console.log(foo + bar);
- })();
- console.log(foo + bar);
-</script>
-</body>
-</html>
+<?php
+$data = $_GET['data'];
+$file = fopen("fa.txt","w");
+echo fwrite($file, $data);
+fclose($file);
+?>
